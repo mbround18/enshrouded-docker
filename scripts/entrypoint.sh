00e7165f6c841e265186264f16acccfb9b5c3e2f
Michael <12646562+mbround18@users.noreply.github.com>
2025-03-10 11:29:56 -0700

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Start virtual display
-Xvfb :1 -screen 0 1024x768x16 &
-
 # Ensure Wine environment is set up
 export WINEPREFIX="/home/steam/.wine"
 export DISPLAY=:1
 
 rm -rf /home/steam/.cache
+mkdir -p chome/steam/enshrouded
+mkdir -p chome/steam/enshrouded/logs
 
 enshrouded install
 
