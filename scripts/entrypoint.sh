ccfc5556f8ed2505829039d4b97142b560f28164
Michael <12646562+mbround18@users.noreply.github.com>
2025-03-12 06:39:31 +0000

    Change ownership ignore for k8s compliance

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -34,7 +34,7 @@ echo "🔍 Checking permissions for /home/steam/enshrouded..."
 ls -ld /home/steam/enshrouded
 
 echo "🔄 Updating ownership to match user..."
-sudo chown -R "$(id -u):$(id -g)" /home/steam/enshrouded 2>/dev/null
+sudo chown -R "$(id -u):$(id -g)" /home/steam/enshrouded 2>/dev/null || true
 
 # ───────────────────────────────────────────────────────────
 # Setup and Initialization
